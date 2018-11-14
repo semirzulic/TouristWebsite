66582326ee3f0db5e2cefdfea65c06085d12d2ad
neskaSpahic <nesibaspahic1997@gmail.com>
2018-11-14 11:59:47 +0100

    "I made page for Attractions of Tuzla"

--- tourist-app/src/app/app.module.ts
+++ tourist-app/src/app/app.module.ts
@@ -5,24 +5,34 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { SectionPopularComponent } from './components/section-popular/section-popular.component';
 import { PopularComponent } from './components/popular/popular.component';
 import { GaleryComponent } from './components/galery/galery.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { HistoryComponent } from './components/components-history/history/history.component';
+import { AppRoutingModule } from './app-routing-history.module';
+import { AttractionsComponent } from './components/components-attractions/attractions/attractions.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
+import { AgmCoreModule } from '@agm/core';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     SliderComponent,
     FooterComponent,
-    SectionPopularComponent,
     PopularComponent,
     GaleryComponent,
-    ContactComponent
+    ContactComponent,
+    HistoryComponent,
+    AttractionsComponent,
+    
+    
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
