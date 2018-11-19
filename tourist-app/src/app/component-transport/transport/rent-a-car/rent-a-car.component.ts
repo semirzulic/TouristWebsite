0fb8ed9eb008450cb7dd9d7ebcd2906f29473224
EmirHusanovic <emirhusanovic@outlook.com>
2018-11-19 10:20:56 +0100

    culture final

--- tourist-app/src/app/component-transport/transport/rent-a-car/rent-a-car.component.ts
+++ tourist-app/src/app/component-transport/transport/rent-a-car/rent-a-car.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-rent-a-car',
@@ -8,47 +7,9 @@ import { formatDate } from '@angular/common';
 })
 export class RentACarComponent implements OnInit {
 
-  num: number = 0;
-  openedPerfect: boolean = false;
-  openedIntercar: boolean = false;
-  openedDalton: boolean = false;
-  time: any;
-  day: any;
-  workDays: string[] = ['Mon','Tue','Wed','Thu','Fri','Sat'];
-
-
   constructor() { }
 
   ngOnInit() {
-
-    this.openedPerfect = this.checkOpened('14:00');
-    this.openedIntercar = this.checkOpened('15:00');
-}
-
-checkOpened(timeEndSunday:any) {
-  for(var i = 0; i < this.workDays.length; i++)
-  {
-  this.day = formatDate(Date.now(), 'EEE', 'en');
-  if(this.day == this.workDays[i])
-      {
-        this.time = formatDate(Date.now(), 'HH:mm', 'en');
-  if(this.day != 'Sat')
-  {
-    if(this.time >= '08:00' && this.time <= '17:00')
-    return true;
-    else
-    return false;
   }
-  else
-  {
-    if(this.time >= '08:00' && this.time <= timeEndSunday)
-    return true;
-    else
-    return false;
-  }
-}
-}
-}
-
 
 }
