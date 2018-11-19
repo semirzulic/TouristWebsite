a82e11053fbdd264164e695dbe798856616cfa1b
neskaSpahic <nesibaspahic1997@gmail.com>
2018-11-19 13:57:39 +0100

    "last version"

---
+++ src/app/virtual-tour/city-map/city-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-city-map',
+  templateUrl: './city-map.component.html',
+  styleUrls: ['./city-map.component.css']
+})
+export class CityMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
